018abd8a718a2cb1ed067aeb4eed2a9e7d6202ef
Domantas-G <78907212+Domantas-G@users.noreply.github.com>
2023-08-16 09:26:01 +0300

    Saving basics

---
+++ Learning/useful_snippets.py
@@ -0,0 +1,12 @@
+# FOr removing Function place dependency and executing the whole code with a single function. 
+def main ():
+  × = int (input ("What's x? ")) 
+  if is even(x):
+    print("Even")
+  else:
+    print ("Odd")
+
+def is even(n):
+  return n % 2 == 0
+
+main()
